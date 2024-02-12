b12bf16bba9ef458d7f9c7c06b4289a8bb72019a
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2024-02-12 18:59:01 +0000

    add get current and forecast weather info api

--- project1/libs/php/getWeatherInfo.php
+++ project1/libs/php/getWeatherInfo.php
@@ -1,54 +1,31 @@
 <?php
-
-// Remove for production
+// remove for production
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 
+$url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'.$_REQUEST['country'].'?unitGroup=us&include=days&key=2RHUYYRXCAGD9UE8968UBGD7H&contentType=json';
 
-$url = 'https://api.openweathermap.org/data/2.5/weather?q=' . urlencode($_REQUEST['country']) . '&appid=ae8183305930109bde697b966d0122af';
-
-// Initialize cURL session
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL,$url);
 
-// Execute the cURL request
 $result = curl_exec($ch);
 
-// Close the cURL session
 curl_close($ch);
 
-// Decode the JSON response
-$decodedResult = json_decode($result, true);
+$decode = json_decode($result,true);
 
-// Check if decoding was successful and the required data is present
-if ($decodedResult !== null && isset($decodedResult['name'])) {
-    // Prepare the response data
-    $output['status']['code'] = "200";
-    $output['status']['name'] = "ok";
-    $output['status']['description'] = "success";
-    $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-    $output['data'] = $decodedResult; 
-} else {
-    // If decoding failed or required data is missing, set an error response
-    $output['status']['code'] = "400";
-    $output['status']['name'] = "error";
-    $output['status']['description'] = "Failed to retrieve valid data from OpenWeatherMap API";
-}
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode;
 
-// Set the response header only if no output has been sent yet
-if (!headers_sent()) {
-    // Set the response header
-    header('Content-Type: application/json; charset=UTF-8');
-}
+// Set the response header
+header('Content-Type: application/json; charset=UTF-8');
 
-// Output the response JSON
-echo json_encode($output);
-
+echo json_encode($output); 
 ?>
-
-
-
