a617299fd7c46b32c104b250c160200eb7ab5d47
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2024-02-27 16:42:46 +0000

    initial project commit

---
+++ project2/libs/php/config.php
@@ -0,0 +1,22 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+
+	$cd_userName = "root";
+	$cd_password = "";
+
+
+	$cd_dbname = "companydirectory";
+	// $cd_user = "companydirectory";
+	// $cd_password = "companydirectory";
+
+
+?>
+
+
