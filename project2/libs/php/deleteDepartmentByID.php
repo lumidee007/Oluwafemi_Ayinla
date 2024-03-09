2bd21f9252b2b531fc3729b92d80258579c14e38
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2024-03-09 13:37:38 +0000

    add delete personnel by id and delete location by id api route

--- project2/libs/php/deleteDepartmentByID.php
+++ project2/libs/php/deleteDepartmentByID.php
@@ -1,10 +1,5 @@
 <?php
 
-	// example use from browser
-	// use insertDepartment.php first to create new dummy record and then specify it's id in the command below
-	// http://localhost/companydirectory/libs/php/deleteDepartmentByID.php?id=<id>
-
-	// remove next two lines for production
 	
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
@@ -15,7 +10,9 @@
 
 	header('Content-Type: application/json; charset=UTF-8');
 
-	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+	$conn = new mysqli($cd_host, $cd_userName, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+	
 
 	if (mysqli_connect_errno()) {
 		
@@ -33,8 +30,6 @@
 
 	}	
 
-	// SQL statement accepts parameters and so is prepared to avoid SQL injection.
-	// $_REQUEST used for development / debugging. Remember to change to $_POST for production
 
 	$query = $conn->prepare('DELETE FROM department WHERE id = ?');
 	
--- project2/libs/php/getAllDepartments.php
+++ project2/libs/php/getAllDepartments.php
@@ -36,7 +36,7 @@
 
 	// SQL does not accept parameters and so is not prepared
 
-	$query = 'SELECT d.id, d.name, l.name as location FROM department d LEFT JOIN location l ON (l.id=d.locationID) ORDER BY d.name';
+	$query = 'SELECT d.id, d.name, d.locationID, l.name as location FROM department d LEFT JOIN location l ON (l.id=d.locationID) ORDER BY d.name';
 
 	$result = $conn->query($query);
 	
